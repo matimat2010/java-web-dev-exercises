0b952a1c6e800d0a95683f9ecc9df970e8fdec20
Matt Alvarado <matimat2010@gmail.com>
2020-01-23 18:30:08 -0600

    8.6. Exercises: Interfaces and Polymorphism Completed

---
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -0,0 +1,27 @@
+package org.launchcode.java.demos.lsn7interfaces;
+
+import java.util.Comparator;
+
+
+
+ public class ConeComparator implements Comparator<Cone> {
+//     @Override
+//     public int compare(Cone o1, Cone o2) {
+//         return Double.compare(o1.getCost(), o2.getCost());
+//     }
+
+
+     @Override
+     public int compare(Cone o1, Cone o2) {
+         if (o1.getCost() < o2.getCost()){
+             return -1;
+         }else if (o1.getCost() > o2.getCost()){
+             return 1;
+         } else {return 0; }
+
+     }
+
+
+ }
+
+
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,19 +1,33 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparator = new FlavorComparator();
+        Comparator cComparator = new ConeComparator();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
-
+        flavors.sort(comparator);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
-
+        cones.sort(cComparator);
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+
+        for(Flavor name : flavors) {
+            System.out.println(name.getName());
+        }
+
+        System.out.println("\n");
+
+        for(Cone cost : cones){
+            System.out.println(cost.getName() + "  " + cost.getCost());
+        }
     }
 }
